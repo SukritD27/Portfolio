1173c929ff9df7962e94a2f1f7130a605ca40266
SukritD27 <sdhawan@sfsu.edu>
2024-04-18 15:34:15 -0700

    The start of a long journey

---
+++ my-portfolio/src/app/page.js
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import {BsFillMoonStarsFill} from 'react-icons/bs';
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <nav>
+        <h1 className="text-xl items-center justify-between font-mono ">Sukrit Dev Dhawan</h1>
+        <ul className="flex items-centre">
+          <li>
+            <BsFillMoonStarsFill className=""cursor-pointer text-2xl></BsFillMoonStarsFill>
+          </li>
+          <li>
+            <a href="../public/Dhawan-Sukrit-Resume.pdf" download="Dhawan-Sukrit-Resume.pdf">Resume</a>
+          </li>
+        </ul>
+      </nav>
+        
+      
+    </main>
+  );
+}
